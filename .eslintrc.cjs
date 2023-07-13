7166a16c7d31c3bd00f0efeb7e3107ae2e16ab42
sumonbarai <sumonbarai78@gmail.com>
2023-07-13 13:12:20 +0600

    eslint modify

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,8 @@ module.exports = {
   rules: {
     "react-refresh/only-export-components": [
       "warn",
-      { allowConstantExport: true, "react/prop-types": false },
+      { allowConstantExport: true },
     ],
   },
+  "react/prop-types": "off",
 };
